9a770b30f2a27031362e219f4b4c083545283ae8
vyshnav <vyshnavravi599@gmail.com>
2023-07-07 20:20:48 +0530

    updated

--- src/components/timeline/timeline.jsx
+++ src/components/timeline/timeline.jsx
@@ -7,30 +7,104 @@ import 'aos/dist/aos.css'; // You can also use <link> for styles
 // ..
 AOS.init();
 
+function Line(){
+    const [scrollState , setScrollState ] = useState(0)
+    let scrollValue = ''
+    let scrolled = 'scrolled'
+    let scroll =0;
+    const handleScroll = ()=>{
+        setScrollState(window.scrollY)
+    }
+    // if(scrollState >500){
+    //      scrollValue = 'scroll'
+    // }
+    // else{
+    //      scrollValue = ''
+    // }
+    {
+    scrollState >= 100 && scrollState <200 ? scrollValue = 'base_line_1' : scrollState >= 200 && scrollState <300 ? scrollValue = 'base_line_2' :scrollState >= 300 && scrollState <400?  scrollValue = 'base_line_3' :    scrollState >= 400 && scrollState <500? scrollValue = 'base_line_4' : scrollState >= 500 && scrollState <600? scrollValue = 'base_line_5' :scrollState >= 600 && scrollState<700 ? scrollValue = 'base_line_6': scrollState >= 700 && scrollState<800 ? scrollValue = 'base_line_7': scrollState >= 800 && scrollState<900 ? scrollValue = 'base_line_8':scrollState >=900 && scrollState <1000 ? scrollValue = 'base_line_9': scrollValue = ''}
+
+    window.addEventListener('scroll',handleScroll)
+    return (
+        <>
+    
+
+        
+        
+        <div className={`content-center bg-blue-800 border-[1px] border-red-300 ${scrolled} ${scrollValue}`}></div>
+        
+     
+        </>
+
+    );
+   
+}
+
 function Timeline() {
-     const [scrollState , setScrollState ] = useState(0)
-     let scrollValue = ''
-     let scrolled = 'scrolled'
-     let scroll =0;
-     const handleScroll = ()=>{
-         setScrollState(window.scrollY)
-     }
-     // if(scrollState >500){
-     //      scrollValue = 'scroll'
-     // }
-     // else{
-     //      scrollValue = ''
-     // }
-     {
-     scrollState >= 100 && scrollState <200 ? scrollValue = 'base_line_1' : scrollState >= 200 && scrollState <300 ? scrollValue = 'base_line_2' :scrollState >= 300 && scrollState <400?  scrollValue = 'base_line_3' :    scrollState >= 400 && scrollState <500? scrollValue = 'base_line_4' : scrollState >= 500 && scrollState <600? scrollValue = 'base_line_5' :scrollState >= 600 && scrollState<700 ? scrollValue = 'base_line_6': scrollState >= 700 && scrollState<800 ? scrollValue = 'base_line_7': scrollState >= 800 && scrollState<900 ? scrollValue = 'base_line_8':scrollState >=900 && scrollState <1000 ? scrollValue = 'base_line_9': scrollValue = ''}
+    
  
   return (
-window.addEventListener('scroll',handleScroll),
- <div className='flex justify-center main-div '>
-     <div className={`self-stretch bg-blue-800 w-o border-[1px] border-red-300  ${scrolled}  ${scrollValue}`}></div>
-     {/* <h1 className='text-white  top-[500px] fixed'>{scrollState}</h1> */}
-    <div className='parent'>
-     <div  data-aos="zoom-out" className='div1 text-white   absolute left-[700px] '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni aliquid, libero facilis excepturi consectetur corporis, laudantium soluta voluptates earum ipsum tenetur amet ipsam sint, molestias blanditiis quae vero sed et?</div>
+  
+
+    <>     {/* <h1 className='text-white  top-[500px] fixed'>{scrollState}</h1> */}
+      
+        
+    {/* <div className='parent bg-black'> */}
+    {/* firt div */}
+    <div className='h-full bg-black'>
+       <div className='flex justify-start'>
+        <div className='w-2/5 mt-10 ml-24'>
+            <p className='text-white text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta laudantium voluptatibus nesciunt, nisi commodi ea, consequatur aliquam, cumque sapiente enim alias necessitatibus facilis. Iure sit repellendus, sint repellat a deleniti.</p>
+        </div>
+       </div>
+    {/* second div */}
+       <div className='flex justify-end'>
+        <div className='w-2/5 mt-10 mr-16'>
+            <p className='text-white text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta laudantium voluptatibus nesciunt, nisi commodi ea, consequatur aliquam, cumque sapiente enim alias necessitatibus facilis. Iure sit repellendus, sint repellat a deleniti.</p>
+        </div>
+       </div>
+       {/* third div */}
+       <div className='flex justify-start'>
+        <div className='w-2/5 mt-10 ml-24'>
+            <p className='text-white text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta laudantium voluptatibus nesciunt, nisi commodi ea, consequatur aliquam, cumque sapiente enim alias necessitatibus facilis. Iure sit repellendus, sint repellat a deleniti.</p>
+        </div>
+       </div>
+
+        {/* fourth div */}
+        <div className='flex justify-end'>
+        <div className='w-2/5 mt-10 mr-16'>
+            <p className='text-white text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta laudantium voluptatibus nesciunt, nisi commodi ea, consequatur aliquam, cumque sapiente enim alias necessitatibus facilis. Iure sit repellendus, sint repellat a deleniti.</p>
+        </div>
+       </div>
+
+         {/* fifth div */}
+         <div className='flex justify-start'>
+        <div className='w-2/5 mt-10 ml-24'>
+            <p className='text-white text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta laudantium voluptatibus nesciunt, nisi commodi ea, consequatur aliquam, cumque sapiente enim alias necessitatibus facilis. Iure sit repellendus, sint repellat a deleniti.</p>
+        </div>
+       </div>
+
+         {/* sixth div */}
+         <div className='flex justify-end'>
+        <div className='w-2/5 mt-10 mr-16'>
+            <p className='text-white text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta laudantium voluptatibus nesciunt, nisi commodi ea, consequatur aliquam, cumque sapiente enim alias necessitatibus facilis. Iure sit repellendus, sint repellat a deleniti.</p>
+        </div>
+       </div>
+
+
+
+        
+       
+       
+
+    <Line/>
+    
+
+    </div>
+    
+   
+   
+     {/* <div  data-aos="zoom-out" className='div1 text-white   absolute left-[700px] '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni aliquid, libero facilis excepturi consectetur corporis, laudantium soluta voluptates earum ipsum tenetur amet ipsam sint, molestias blanditiis quae vero sed et?</div>
      <div  data-aos="zoom-out" className='div2 text-white   absolute right-[700px] top-12'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni aliquid, libero facilis excepturi consectetur corporis, laudantium soluta voluptates earum ipsum tenetur amet ipsam sint, molestias blanditiis quae vero sed et?</div>
      <div  data-aos="zoom-out" className='div3 text-white   absolute left-[700px] top-36'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni aliquid, libero facilis excepturi consectetur corporis, laudantium soluta voluptates earum ipsum tenetur amet ipsam sint, molestias blanditiis quae vero sed et?</div>
      <div  data-aos="zoom-out" className='div4 text-white   absolute right-[700px] top-48'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni aliquid, libero facilis excepturi consectetur corporis, laudantium soluta voluptates earum ipsum tenetur amet ipsam sint, molestias blanditiis quae vero sed et?</div>
@@ -43,9 +117,9 @@ window.addEventListener('scroll',handleScroll),
      <div  data-aos="zoom-out" className='div11 text-white   absolute left-[700px] top-[850px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni aliquid, libero facilis excepturi consectetur corporis, laudantium soluta voluptates earum ipsum tenetur amet ipsam sint, molestias blanditiis quae vero sed et?</div>
      <div  data-aos="zoom-out" className='div12 text-white   absolute right-[700px] top-[900px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni aliquid, libero facilis excepturi consectetur corporis, laudantium soluta voluptates earum ipsum tenetur amet ipsam sint, molestias blanditiis quae vero sed et?</div>
     
-     </div>
-     </div>
-   
+    </div> */}
+    </>
+
  )
 }
 
